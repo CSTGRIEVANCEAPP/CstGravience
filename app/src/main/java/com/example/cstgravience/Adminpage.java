5432091a64a8232f92eb5ac2a58349a4d72007f3
Kiran-sawa <kiransawa8@gmail.com>
2022-09-21 15:31:15 +0600

    yoyoy

--- app/src/main/java/com/example/cstgravience/Adminpage.java
+++ app/src/main/java/com/example/cstgravience/Adminpage.java
@@ -1,14 +1,30 @@
 package com.example.cstgravience;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class Adminpage extends AppCompatActivity {
+    ViewPager2 viewPager2;
+    TabLayout tabLayout;
+    myadapter myadapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adminpage);
+
+        viewPager2=findViewById(R.id.viewpage);
+        tabLayout=findViewById(R.id.tablayout);
+        viewPager2.setAdapter(myadapter);
+
+        yab
+
+
+
     }
 }
--- app/src/main/java/com/example/cstgravience/myadapter.java
+++ app/src/main/java/com/example/cstgravience/myadapter.java
@@ -1,14 +1,39 @@
 package com.example.cstgravience;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import android.os.Bundle;
+import com.example.cstgravience.fragments.Hostel;
+import com.example.cstgravience.fragments.Mess;
+import com.example.cstgravience.fragments.Personal;
+import com.example.cstgravience.fragments.academic;
 
-public class myadapter extends AppCompatActivity {
 
+public class myadapter extends FragmentStateAdapter {
+
+    public myadapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_myadapter);
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+
+                return new academic();
+            case 1:
+                return new Hostel();
+            case 2:
+                return new Mess();
+            default:
+                return new Personal();
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return 4;
     }
 }
