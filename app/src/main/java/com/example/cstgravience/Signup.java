e98a7752f48cc6d554662772851aa62f63223701
Ugyen Thinley Dorjee <87816434+UT01000100@users.noreply.github.com>
2022-10-07 18:25:21 +0600

    database for registration under process

--- app/src/main/java/com/example/cstgravience/Signup.java
+++ app/src/main/java/com/example/cstgravience/Signup.java
@@ -7,20 +7,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Signup extends AppCompatActivity {
     Button button;
-
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         button=findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(Signup.this,Adminpage.class);
-                startActivity(intent);
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent=new Intent(Signup.this,Adminpage.class);
+//                startActivity(intent);
+//            }
+//        });
     }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        FirebaseUser user = mAuth.getCurrentUser();
+//        if (user == null){
+//            startActivity(new Intent(MainActivity.this,Adminpage.class));
+//        }
+//    }
 }
