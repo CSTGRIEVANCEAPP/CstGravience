ac8c7a3755adb388dfd5d2ff3eae0ada5873ca38
TenzinJams <tenzinjamtsho108@gmail.com>
2022-10-14 00:31:25 +0530

    Adding text grievances to realtime database

---
+++ app/src/main/java/com/example/cstgravience/HelperClass.java
@@ -0,0 +1,21 @@
+package com.example.cstgravience;
+
+public class HelperClass {
+    String Grievance;
+
+    public HelperClass(){
+
+    }
+
+    public HelperClass(String grievance) {
+        Grievance = grievance;
+    }
+
+    public String getGrievance() {
+        return Grievance;
+    }
+
+    public void setGrievance(String grievance) {
+        Grievance = grievance;
+    }
+}
