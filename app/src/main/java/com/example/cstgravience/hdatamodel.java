cc50a1e65d64b427a10a07c1218a0788e77b2862
TenzinJams <tenzinjamtsho108@gmail.com>
2022-10-15 15:36:45 +0530

    Hostel grievance view for admin

---
+++ app/src/main/java/com/example/cstgravience/hdatamodel.java
@@ -0,0 +1,22 @@
+package com.example.cstgravience;
+
+public class hdatamodel {
+    String grievance;
+
+    public hdatamodel() {
+    }
+
+    public hdatamodel(String grievance) {
+
+        this.grievance = grievance;
+    }
+
+    public String getGrievance() {
+        return grievance;
+    }
+
+    public void setGrievance(String grievance) {
+        this.grievance = grievance;
+    }
+}
+
