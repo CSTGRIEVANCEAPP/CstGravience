c4594ce43b369d443c4a5707db99f12446badf78
Ugyen Thinley Dorjee <87816434+UT01000100@users.noreply.github.com>
2022-10-16 12:04:23 +0600

    user delete

--- app/src/main/java/com/example/cstgravience/fragments/Profile.java
+++ app/src/main/java/com/example/cstgravience/fragments/Profile.java
@@ -1,9 +1,11 @@
 package com.example.cstgravience.fragments;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,12 +13,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cstgravience.R;
+import com.example.cstgravience.Signup;
 import com.example.cstgravience.UserLogin;
 import com.example.cstgravience.change_password;
 import com.example.cstgravience.model.Users;
 import com.example.cstgravience.resetpassword;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -30,10 +36,10 @@ import java.nio.file.attribute.UserPrincipalLookupService;
 
 
 public class Profile extends Fragment {
-    DatabaseReference reference;
+
     FirebaseUser user;
     TextView userinfo;
-    Button changepasswordbtn;
+    Button changepasswordbtn,signout;
 
 
     @Override
@@ -43,7 +49,8 @@ public class Profile extends Fragment {
        View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
        changepasswordbtn = view.findViewById(R.id.changepassword);
-       userinfo = view.findViewById(R.id.userinfo);
+       user=FirebaseAuth.getInstance().getCurrentUser();
+       signout=view.findViewById(R.id.signout);
        changepasswordbtn.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
@@ -51,6 +58,39 @@ public class Profile extends Fragment {
                startActivity(intent);
            }
        });
+       signout.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+               alert.setTitle("Alert!")
+                       .setMessage("By signing out you will permanently delete your account")
+                       .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                           @Override
+                           public void onClick(DialogInterface dialog, int which) {
+                               user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                   @Override
+                                   public void onComplete(@NonNull Task<Void> task) {
+                                       if(task.isSuccessful()){
+                                           Toast.makeText(getContext(), "Your Account has been deleted", Toast.LENGTH_SHORT).show();
+                                           Intent intent = new Intent(getActivity(), Signup.class);
+                                           startActivity(intent);
+                                       }else{
+                                           Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                       }
+                                   }
+                               });
+                           }
+                       });
+               alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+                       dialog.cancel();
+                   }
+               });
+               alert.show();
+           }
+       });
+
         return view;
 
 
